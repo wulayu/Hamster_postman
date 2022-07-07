a679b5302ad455eed1be65c6f234cb8aa52650ec
soong <849519573@qq.com>
2022-07-07 14:29:03 +0800

    test1_rembg output

--- test/rembg/test_output.py
+++ test/rembg/test_output.py
@@ -1,9 +1,12 @@
 from rembg import remove
 import os
 import cv2
+import time
 
 source = "../../source/"
 out_path = "../../output/rembg/"
+
+time_start = time.time()
 
 for file_name in os.listdir(source):
     input_path = file_name
@@ -11,16 +14,13 @@ for file_name in os.listdir(source):
     # b = file_name.partition('.')[2]
     output_path = 'output_' + file_name
 
-    print(input_path)
-    print(output_path)
-    print()
+    # print(input_path)
+    # print(output_path)
+    # print()
 
     input = cv2.imread(os.path.join(source, input_path))
     output = remove(input)
     cv2.imwrite(os.path.join(out_path, output_path), output)
 
-
-
-
-
-
+time_end = time.time()
+print('time cost', time_end - time_start, 's')
