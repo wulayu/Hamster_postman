25bd3dfdf9c15ef294f8adc932c6036bc15fb533
soong <849519573@qq.com>
2022-07-08 12:23:56 +0800

    test1_rembg output + time test2_image output + time

--- test/image-background-remove-tool/generator_ima_back_rem.py
+++ test/image-background-remove-tool/generator_ima_back_rem.py
@@ -9,17 +9,35 @@ from carvekit.pipelines.postprocessing import MattingMethod
 from carvekit.pipelines.preprocessing import PreprocessingStub
 from carvekit.trimap.generator import TrimapGenerator
 
+import xlsxwriter
+import sys
+import importlib
+importlib.reload(sys)
+
 source = "../../source/"
 out_path = "../../output/image-background-remove-tool/"
+filename = 'time_order.xlsx'
+colums_name = ['序号', '图片名称', '图片类型', '处理时间']
 if not os.path.isdir(out_path):
     os.mkdir(out_path)
 
-time_start = time.time()
+book = xlsxwriter.Workbook(filename)
+sh = book.add_worksheet('sheet1')
+sh.set_column("B:B", 93.67)
+sh.set_column("D:D", 22.33)
+sh.set_column("A:A", 10.33)
+sh.set_column("C:C", 20)
+sh.write_row('A1', colums_name)
+total_time = 0
 
 for index, file_name in enumerate(os.listdir(source)):
-    if file_name != '02-927.jpg':
-        continue
+    time_start = time.time()
+
+    # if file_name != '02-927.jpg':
+    #     continue
     input_path = file_name
+    a = file_name.partition('.')[0]
+    b = file_name.partition('.')[2]
     output_path = 'output_' + str(index) + '.png'
 
     u2net = U2NET(device='cpu', batch_size=1)
@@ -34,7 +52,18 @@ for index, file_name in enumerate(os.listdir(source)):
     cat_wo_bg = interface([image])[0]
     cat_wo_bg.save(os.path.join(out_path, output_path), format='png')
 
-    print(str(index + 1) + '/' + str(len(os.listdir(source))) + '\n')
+    time_end = time.time()
+    print(input_path)
+    print(output_path)
+    print(str(index + 1) + '/' + str(len(os.listdir(source))))
+    print(index + 1, ' time cost', time_end - time_start, ' s', '\n')
+    total_time += time_end - time_start;
 
-time_end = time.time()
-print('time cost', time_end - time_start, 's')
+    data = [index + 1, a, b, str(time_end - time_start) + ' s']
+    sh.write_row('A' + str(index + 2), data)
+
+data = ['总时间：', str(total_time) + ' s']
+sh.write_row('C' + str(index + 3), data)
+data = ['平均时间：', str(total_time / (index + 1)) + ' s']
+sh.write_row('C' + str(index + 4), data)
+book.close()
--- test/rembg/generator_rembg.py
+++ test/rembg/generator_rembg.py
@@ -3,29 +3,54 @@ import os
 import time
 from PIL import Image
 
+import xlsxwriter
+import sys
+import importlib
+importlib.reload(sys)
+
 source = "../../source/"
 out_path = "../../output/rembg/"
+filename = 'time_order.xlsx'
+colums_name = ['序号', '图片名称', '图片类型', '处理时间']
 if not os.path.isdir(out_path):
     os.mkdir(out_path)
 
-time_start = time.time()
+book = xlsxwriter.Workbook(filename)
+sh = book.add_worksheet('sheet1')
+sh.set_column("B:B", 93.67)
+sh.set_column("D:D", 22.33)
+sh.set_column("A:A", 10.33)
+sh.set_column("C:C", 20)
+sh.write_row('A1', colums_name)
+total_time = 0
 
 for index, file_name in enumerate(os.listdir(source)):
-    # if file_name != '02-927.jpg':
+    time_start = time.time()
+
+    # if file_name != '02-927.jpg' and file_name != '98150-22VX_F.webp':
     #     continue
     input_path = file_name
-    # a = file_name.partition('.')[0]
-    # b = file_name.partition('.')[2]
+    a = file_name.partition('.')[0]
+    b = file_name.partition('.')[2]
     output_path = 'output_' + str(index) + '.png'
-
-    print(input_path)
-    print(output_path)
-    print(str(index + 1) + '/' + str(len(os.listdir(source))) + '\n')
 
     _input = Image.open(os.path.join(source, input_path))
     _input = _input.convert(mode='RGBA')
     output = remove(_input)
     output.save(os.path.join(out_path, output_path))
 
-time_end = time.time()
-print('time cost', time_end - time_start, 's')
+    time_end = time.time()
+    print(input_path)
+    print(output_path)
+    print(str(index + 1) + '/' + str(len(os.listdir(source))))
+    print(index + 1, ' time cost', time_end - time_start, ' s', '\n')
+    total_time += time_end - time_start;
+
+    data = [index + 1, a, b, str(time_end - time_start) + ' s']
+    sh.write_row('A' + str(index + 2), data)
+
+data = ['总时间：', str(total_time) + ' s']
+sh.write_row('C' + str(index + 3), data)
+data = ['平均时间：', str(total_time / (index + 1)) + ' s']
+sh.write_row('C' + str(index + 4), data)
+book.close()
